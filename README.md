dba67b7d74f499d40da8a0a2f39382b863af0a11
Ido Flax <idoflax@gmail.com>
2023-09-15 15:41:49 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,36 @@
+## Execute gradle tasks on projects
+
+This GitHub Action allows you to detect modified subprojects in a monorepo, this can be  
+
+### Inputs
+
+-  `project_prefixes`  (required): prefixes of the names of the projects in question, comma-separated.
+
+### Outputs
+
+-  `modified_projects` : the detected modified projects.
+
+### Example Usage
+```yaml
+name: Detect modified subprojects
+description: Detect modified subprojects
+
+on:
+  push:
+    branches:
+      - main
+
+jobs:
+  build:
+  runs-on: ubuntu-latest
+
+  steps:
+    - name: Checkout repository
+      uses: actions/checkout@v2
+
+    - name: Detect modified subprojects
+      uses: ./.github/actions/detect-modified-projects
+      id: detect-modified-subprojects
+      with:
+        project_prefixes: my-prefix,his-prefix
+```
---
+++ src/index.ts
@@ -0,0 +1,49 @@
+import {execSync} from 'child_process';
+import * as core from '@actions/core';
+
+function run() {
+    try {
+        const subprojectPrefixes = core.getInput('project_prefixes')?.split(",") ?? [];
+
+        core.debug("executing git fetch");
+        execSync('git fetch');
+
+        const githubSha = process.env.GITHUB_SHA;
+        if (!githubSha) {
+            core.setFailed('GITHUB_SHA not set')
+        }
+        const diffCmd = `git diff --name-only origin/main..${githubSha}`
+        core.debug("Calling: " + diffCmd);
+        let modifiedProjects = execSync(diffCmd, {encoding: 'utf8'});
+        core.debug("Result:" + modifiedProjects)
+        if (modifiedProjects.includes('buildSrc/') && !modifiedProjects.includes('ktor-')) {
+            core.debug("only buildSrc has modified");
+            modifiedProjects = "buildSrc";
+        } else {
+            const subprojectPrefixesPattern = subprojectPrefixes.join("|")
+            core.debug("subprojectPrefixesPattern: " + subprojectPrefixesPattern);
+            const regex = subprojectPrefixes.length > 0
+                ? new RegExp(`^(${subprojectPrefixesPattern})`)
+                : null;
+            modifiedProjects = modifiedProjects.split('\n')
+                .filter(line => {
+                    return regex ? regex.test(line) : true;
+                })
+                .map(line => line.split('/', 1)[0])
+                .sort()
+                .filter((value, index, self) => self.indexOf(value) === index)
+                .join(',');
+        }
+        if (modifiedProjects) {
+            core.debug(`Modified subprojects: ${modifiedProjects}`);
+            core.setOutput('modified_projects', modifiedProjects);
+        } else {
+            core.info("No modified subprojects");
+        }
+
+    } catch (error) {
+        core.setFailed(`Action failed with error: ${error}`);
+    }
+}
+
+run();
